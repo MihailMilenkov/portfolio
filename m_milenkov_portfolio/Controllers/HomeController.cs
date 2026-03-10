366336bf19e2d3b4cf905cfdc753bc9b2c5d3380
Mihail Milenkov <mihailmilenkov681@gmail.com>
2026-03-10 16:42:04 +0200

    Modified Index.cshtml:  Modified contacts section, added email with copy function, phone, github and linkedin links, opened in another tab.  Modified experience timeline.  Modified projects.
    Added WorkInProgress.cshtml view to be shown when links are clicked and the content is not implemented.
    
    Modified _Layout.cshtml
     Simplified footer.
     Added function for copy email button.
    
    Modified site.css
     Added .no-link-style
     Added .contacts-link
    
    Modified HomeController.cs
     Added WorkInProgress method to return View() and change the view to WorkInProgress.cshtml

--- m_milenkov_portfolio/Controllers/HomeController.cs
+++ m_milenkov_portfolio/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace m_milenkov_portfolio.Controllers
             return View();
         }
 
+        public IActionResult WorkInProgress()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
